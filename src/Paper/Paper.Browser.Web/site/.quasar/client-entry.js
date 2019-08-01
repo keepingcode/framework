0cc4b03d67086418f5b082ec012409b4578bcd73
francine <francinescosta@gmail.com>
2019-08-01 11:06:30 -0300

    Atualizacoes e melhorias do projeto

---
+++ src/Paper/Paper.Browser.Web/site/.quasar/client-entry.js
@@ -0,0 +1,111 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.conf.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+
+import '@quasar/extras/material-icons/material-icons.css'
+
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+
+
+
+// We load Quasar stylus files
+import 'quasar/dist/quasar.styl'
+
+
+
+
+import 'src/css/app.styl'
+
+
+import Vue from 'vue'
+import createApp from './app.js'
+
+
+
+
+import qboot_Bootaxios from 'boot/axios'
+
+import qboot_Bootpaper from 'boot/paper'
+
+import qboot_Bootvue2filters from 'boot/vue2-filters'
+
+import qboot_Bootfilters from 'boot/filters'
+
+
+
+
+
+
+
+
+
+Vue.config.devtools = true
+Vue.config.productionTip = false
+
+
+
+console.info('[Quasar] Running SPA.')
+
+
+
+const { app, store, router } = createApp()
+
+
+
+async function start () {
+  
+  const bootFiles = [qboot_Bootaxios,qboot_Bootpaper,qboot_Bootvue2filters,qboot_Bootfilters]
+  for (let i = 0; i < bootFiles.length; i++) {
+    if (typeof bootFiles[i] !== 'function') {
+      continue
+    }
+
+    try {
+      await bootFiles[i]({
+        app,
+        router,
+        store,
+        Vue,
+        ssrContext: null
+      })
+    }
+    catch (err) {
+      if (err && err.url) {
+        window.location.href = err.url
+        return
+      }
+
+      console.error('[Quasar] boot error:', err)
+      return
+    }
+  }
+  
+
+  
+
+    
+
+    
+
+      new Vue(app)
+
+    
+
+  
+
+}
+
+start()
---
+++ src/Paper/Paper.Browser.Web/site/.quasar/import-quasar.js
@@ -0,0 +1,19 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.conf.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+
+import Vue from 'vue'
+
+import {Quasar,QBtn,QBtnDropdown,QCard,QCardSection,QCardActions,QCheckbox,QDate,QDialog,QDrawer,QField,QHeader,QIcon,QInfiniteScroll,QInput,QItem,QItemSection,QItemLabel,QLayout,QList,QMenu,QPageContainer,QPage,QPopupProxy,QSelect,QSeparator,QScrollArea,QSpace,QTable,QTime,QToolbar,QToolbarTitle,QTooltip,QTh,QTr,QTd,Ripple,ClosePopup,Notify,Dialog} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},components: {QBtn,QBtnDropdown,QCard,QCardSection,QCardActions,QCheckbox,QDate,QDialog,QDrawer,QField,QHeader,QIcon,QInfiniteScroll,QInput,QItem,QItemSection,QItemLabel,QLayout,QList,QMenu,QPageContainer,QPage,QPopupProxy,QSelect,QSeparator,QScrollArea,QSpace,QTable,QTime,QToolbar,QToolbarTitle,QTooltip,QTh,QTr,QTd},directives: {Ripple,ClosePopup},plugins: {Notify,Dialog} })
